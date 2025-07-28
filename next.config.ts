4c2c0a324b59cee8efacb3f475ebc61969345b29
Agastya Khati <krishkhati70@gmail.com>
2025-07-28 22:37:28 +0530

    "2.9"

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+// next.config.ts (or .js if you're not using TypeScript)
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // you can add more config options here later
 };
 
 export default nextConfig;
